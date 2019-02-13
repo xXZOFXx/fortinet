bb69abc21d0ea8b9cfed388f464815575c0f2094
xXZOFXx <ricardoyael15@gmail.com>
2019-02-13 12:01:48 -0600

    php script

--- conexion.php
+++ conexion.php
@@ -1,10 +1,11 @@
 <?php
-	$serverName = "18.217.147.215\MSSQLSERVER, 1433"; //serverName\instanceName
+	//Nombre del servidor \nombre de la instancia
+	$serverName = "18.217.147.215\sqlexpress, 1433"; 
 	$connectionInfo = array( "Database"=>"cliente_db", "UID"=>"sa", "PWD"=>"C0m3x@2018");
 	$conn = sqlsrv_connect( $serverName, $connectionInfo);
 
 	if($conn) {
-    	 echo "Conexión establecida.<br>";
+    	//echo "Conexión establecida.<br>";
 	}else{
      	echo "Conexión no se pudo establecer.<br>";
      	die( print_r( sqlsrv_errors(), true));
